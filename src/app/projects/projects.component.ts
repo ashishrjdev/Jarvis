4e9777b00d8778ada07cd73c1931da3d43ab8167
Ashish Ranjan <raashishdev@gmail.com>
2019-06-22 16:01:01 +0530

    Added material icons. Implemented update project functionality

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -10,6 +10,9 @@ import { Project } from '../shared/models/project.model';
 export class ProjectsComponent implements OnInit {
     projects: {id: string, data: Project}[];
     isdataLoaded: boolean = false;
+    isEditing: boolean = false;
+    selectedProject: Project = new Project();
+    selectedPid: string;
     constructor(private projectService: ProjectService) {}
 
     ngOnInit() {
@@ -40,17 +43,30 @@ export class ProjectsComponent implements OnInit {
         this.projectService.deleteProject(projectId);
     }
 
-    public updateProject(projectId: string) {
-        const updateProject = {
-            name: 'Test project' + Math.random().toString(),
-            description: 'This is test project ' + Math.random().toString()
+    public updateProject(projectId: string, updatedName: string, updatedDesc:string) {
+        const updatedProject = {
+            name: updatedName,
+            description: updatedDesc
         };
-        this.projectService.updateProject(projectId, updateProject);
+        this.projectService.updateProject(projectId, updatedProject);
+    }
+
+    public setProject(project: any) {
+        this.selectedPid = project.id;
+        this.selectedProject = project.data;
+        this.isEditing = true;
     }
 
     public onSubmit(form: HTMLFormElement) {
         const formData = form.value;
-        this.addProject(formData.projecttitle, formData.projectdesc);
+        if (this.isEditing) {
+            this.updateProject(this.selectedPid, formData.projecttitle, formData.projectdesc);
+        } else {
+            this.addProject(formData.projecttitle, formData.projectdesc);
+        }
+        this.isEditing = false;
+        this.selectedPid = "";
+        this.selectedProject = new Project();
         form.reset();
     }
 }
